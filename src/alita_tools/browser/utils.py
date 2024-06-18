4b0bc5d8bec41100429984803ec1d219bf560d39
arozumenko <artyom.rozumenko@gmail.com>
2024-06-18 21:15:10 +0300

    handling html parser

--- src/alita_tools/browser/utils.py
+++ src/alita_tools/browser/utils.py
@@ -1,3 +1,4 @@
+import re
 from langchain_community.document_loaders import AsyncChromiumLoader
 from langchain_community.document_transformers import BeautifulSoupTransformer
 from langchain.text_splitter import CharacterTextSplitter
@@ -7,13 +8,21 @@ from langchain_chroma import Chroma
 from langchain_community.embeddings.sentence_transformer import (
     SentenceTransformerEmbeddings,
 )
+from json import dumps
 
 # retrieves pages and extracts text by tag
 def get_page(urls, html_only=False):
     loader = AsyncChromiumLoader(urls)
     html = loader.load()
     if html_only:
-        return html
+        body = []
+        # Regular expression to match <style></style> and <script></script> tags and their content
+        regex_pattern = r'<style.*?</style>|<script.*?</script>'
+        for ht in html:
+            # Using re.sub to remove the matched content
+            cleaned_html = re.sub(regex_pattern, '', ht.page_content, flags=re.DOTALL)
+            body.append(cleaned_html)
+        return "\n\n".join(body)
     bs_transformer = BeautifulSoupTransformer()
     docs_transformed = bs_transformer.transform_documents(html, tags_to_extract=["p"], remove_unwanted_tags=["a"])
     return docs_transformed
