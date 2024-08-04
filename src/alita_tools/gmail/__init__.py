b19c5ccdfe511c2b25820be2e1cf3824ee306caa
Andrei_Nikiporchyk <andrei_nikiporchyk@epam.com>
2024-08-05 00:00:22 +0400

    langchain gmail tool

--- src/alita_tools/gmail/__init__.py
+++ src/alita_tools/gmail/__init__.py
@@ -3,30 +3,19 @@ from typing import List
 from langchain_core.tools import BaseToolkit
 from langchain_core.tools import BaseTool
 
-from .yagmail_wrapper import YagmailWrapper
-from ..base.tool import BaseAction
+from .gmail_wrapper import GmailWrapper
 
-class AlitaYagmailToolkit(BaseToolkit):
+
+class AlitaGmailToolkit(BaseToolkit):
     tools: List[BaseTool] = []
-    
-    @classmethod
-    def get_toolkit(cls, selected_tools: list[str] | None = None, **kwargs):
-        if selected_tools is None:
-            selected_tools = []
-        yagmail_wrapper = YagmailWrapper(**kwargs)
-        available_tools = yagmail_wrapper.get_available_tools()
-        tools = []
+
+    def get_toolkit(self, api_resource, selected_tools: List[str]):
+        gmail_wrapper = GmailWrapper()
+        available_tools = gmail_wrapper._get_available_tools(api_resource)
         for tool in available_tools:
-            if selected_tools:
-                if tool["name"] not in selected_tools:
-                    continue
-            tools.append(BaseAction(
-                api_wrapper=yagmail_wrapper,
-                name=tool["name"],
-                description=tool["description"],
-                args_schema=tool["args_schema"]
-            ))
-        return cls(tools=tools)
+            if selected_tools.__contains__(tool['name']):
+                self.tools.append(tool.get('tool'))
+        return self
 
     def get_tools(self):
         return self.tools
