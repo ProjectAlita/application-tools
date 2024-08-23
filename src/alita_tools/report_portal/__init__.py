f87da7a2d3bd370bff213c0e09abd2c66916b5a9
Jallermax <jallermax@gmail.com>
2024-08-23 18:17:08 +0400

    Add report_portal init for package

--- src/alita_tools/report_portal/__init__.py
+++ src/alita_tools/report_portal/__init__.py
@@ -3,6 +3,17 @@ from langchain_core.tools import BaseToolkit, BaseTool
 from .api_wrapper import ReportPortalApiWrapper
 from ..base.tool import BaseAction
 
+name = "report_portal"
+
+
+def get_tools(tool):
+    return ReportPortalToolkit().get_toolkit(
+        selected_tools=tool['settings'].get('selected_tools', []),
+        endpoint=tool['settings']['endpoint'],
+        api_key=tool['settings']['api_key'],
+        project=tool['settings']['project']
+    ).get_tools()
+
 
 class ReportPortalToolkit(BaseToolkit):
     tools: list[BaseTool] = []
@@ -27,4 +38,3 @@ class ReportPortalToolkit(BaseToolkit):
 
     def get_tools(self) -> list[BaseTool]:
         return self.tools
-
