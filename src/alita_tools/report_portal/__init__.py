f7ad9f311cfff22b7d3254b42e010464f094807a
Jallermax <jallermax@gmail.com>
2024-08-23 17:12:40 +0400

    Add ReportPortal ToolKit with 10 retrieval tools

---
+++ src/alita_tools/report_portal/__init__.py
@@ -0,0 +1,30 @@
+from langchain_core.tools import BaseToolkit, BaseTool
+
+from .api_wrapper import ReportPortalApiWrapper
+from ..base.tool import BaseAction
+
+
+class ReportPortalToolkit(BaseToolkit):
+    tools: list[BaseTool] = []
+
+    @classmethod
+    def get_toolkit(cls, selected_tools: list[str] | None = None, **kwargs):
+        if selected_tools is None:
+            selected_tools = []
+        report_portal_api_wrapper = ReportPortalApiWrapper(**kwargs)
+        available_tools = report_portal_api_wrapper.get_available_tools()
+        tools = []
+        for tool in available_tools:
+            if selected_tools and tool["name"] not in selected_tools:
+                continue
+            tools.append(BaseAction(
+                api_wrapper=report_portal_api_wrapper,
+                name=tool["name"],
+                description=tool["description"],
+                args_schema=tool["args_schema"]
+            ))
+        return cls(tools=tools)
+
+    def get_tools(self) -> list[BaseTool]:
+        return self.tools
+
