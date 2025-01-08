29f8c428a5c9adc41cdabf0f863f7b4233d7c823
Mikhail Campos Guadamuz <Mikhail_Campos-Guadamuz@epam.com>
2025-01-08 17:15:50 +0300

    Add cloud field to bibucket toolkit schema

--- src/alita_tools/bitbucket/__init__.py
+++ src/alita_tools/bitbucket/__init__.py
@@ -1,10 +1,11 @@
-from typing import Dict, List, Literal
+from typing import Dict, List, Literal, Optional
 from .api_wrapper import BitbucketAPIWrapper
 from .tools import __all__
 from langchain_core.tools import BaseToolkit
 from langchain_core.tools import BaseTool
 from pydantic import create_model, BaseModel, ConfigDict
 from pydantic.fields import FieldInfo
+
 
 name = "bitbucket"
 
@@ -16,7 +17,8 @@ def get_tools(tool):
         repository=tool['settings']['repository'],
         username=tool['settings']['username'],
         password=tool['settings']['password'],
-        branch=tool['settings']['branch']
+        branch=tool['settings']['branch'],
+        cloud=tool['settings'].get('cloud')
     ).get_tools()
 
 
@@ -34,6 +36,7 @@ class AlitaBitbucketToolkit(BaseToolkit):
             branch=(str, FieldInfo(description="Main branch", default="main")),
             username=(str, FieldInfo(description="Username")),
             password=(str, FieldInfo(description="GitLab private token", json_schema_extra={'secret': True})),
+            cloud=(Optional[bool], FieldInfo(description="Hosting Option", default=None)),
             selected_tools=(List[Literal[tuple(selected_tools)]], []),
             __config__=ConfigDict(json_schema_extra={'metadata': {"label": "Bitbucket", "icon_url": None}})
         )
@@ -42,8 +45,8 @@ class AlitaBitbucketToolkit(BaseToolkit):
     def get_toolkit(cls, selected_tools: list[str] | None = None, **kwargs):
         if selected_tools is None:
             selected_tools = []
-        if "cloud" not in kwargs and ("bitbucket.org" in kwargs.get('url')):
-            kwargs["cloud"] = True
+        if kwargs["cloud"] is None:
+            kwargs["cloud"] = True if "bitbucket.org" in kwargs.get('url') else False
         bitbucket_api_wrapper = BitbucketAPIWrapper(**kwargs)
         available_tools: List[Dict] = __all__
         tools = []
