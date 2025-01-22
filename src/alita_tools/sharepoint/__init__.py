e5ace694aee2ff9ac7e43db2801742e9090f7605
Mikhail Campos Guadamuz <Mikhail_Campos-Guadamuz@epam.com>
2025-01-22 09:59:01 +0300

    New toolkit_config_schema() for sharepoint toolkit

--- src/alita_tools/sharepoint/__init__.py
+++ src/alita_tools/sharepoint/__init__.py
@@ -1,12 +1,13 @@
-from typing import List
+from typing import List, Literal
 
 from langchain_core.tools import BaseToolkit, BaseTool
-from pydantic import BaseModel, create_model
+from pydantic import BaseModel, create_model, ConfigDict
 from pydantic.fields import FieldInfo
 from .api_wrapper import SharepointApiWrapper
 from ..base.tool import BaseAction
 
 name = "sharepoint"
+
 
 def get_tools(tool):
     return (SharepointToolkit().get_toolkit(
@@ -22,11 +23,14 @@ class SharepointToolkit(BaseToolkit):
 
     @staticmethod
     def toolkit_config_schema() -> BaseModel:
+        selected_tools = (x['name'] for x in SharepointApiWrapper.construct().get_available_tools())
         return create_model(
             name,
             site_url=(str, FieldInfo(description="Sharepoint site's URL")),
             client_id=(str, FieldInfo(description="Client ID")),
-            client_secret=(str, FieldInfo(description="Client Secret"))
+            client_secret=(str, FieldInfo(description="Client Secret", json_schema_extra={'secret': True})),
+            selected_tools=(List[Literal[tuple(selected_tools)]], []),
+            __config__=ConfigDict(json_schema_extra={'metadata': {"label": "Sharepoint", "icon_url": None}})
         )
 
     @classmethod
