d7d07a60677c15ac383d1c7f393bbfca9cf57f8e
arozumenko <artyom.rozumenko@gmail.com>
2025-02-17 19:50:19 +0200

    change search formatting

--- src/alita_tools/code/loaders/codesearcher.py
+++ src/alita_tools/code/loaders/codesearcher.py
@@ -6,6 +6,6 @@ def search_format(items):
         res_chunk = ''
         language = get_programming_language(get_file_extension(doc.metadata["filename"]))
         res_chunk += doc.metadata["filename"] + " -> " + doc.metadata["method_name"] + " (score: " + str(score) + ")"
-        res_chunk += "\n\n```" + language.value + "\n"+ doc.page_content + "\n```"
+        res_chunk += "\n\n```" + language.value + "\n"+ doc.page_content + "\n```\n\n"
         results.append(res_chunk)
     return results
