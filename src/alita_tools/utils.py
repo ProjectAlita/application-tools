8533d6445af81aac0b7970fbe7b0e34342d1be20
mikalai_biazruchka <mikalai_biazruchka@epam.com>
2025-03-04 13:53:31 +0300

    introduce `toolkit_name`

---
+++ src/alita_tools/utils.py
@@ -0,0 +1,13 @@
+import re
+
+TOOLKIT_SPLITTER = "___"
+
+
+def clean_string(s):
+    # This pattern matches characters that are NOT alphanumeric, underscores, or hyphens
+    pattern = '[^a-zA-Z0-9_.-]'
+
+    # Replace these characters with an empty string
+    cleaned_string = re.sub(pattern, '', s)
+
+    return cleaned_string
