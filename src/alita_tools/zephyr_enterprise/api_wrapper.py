eebd7858c149c62bf13bafe2debeb57dc766c70b
nochore <40186790+nochore@users.noreply.github.com>
2025-03-15 14:40:02 +0400

    Feat(158): Introduce create-update step for Zephyr Enterprise

--- src/alita_tools/zephyr_enterprise/api_wrapper.py
+++ src/alita_tools/zephyr_enterprise/api_wrapper.py
@@ -12,16 +12,15 @@ class ZephyrApiWrapper(BaseToolApiWrapper):
     token: str
     _client: Optional[ZephyrClient] = PrivateAttr()
 
-    @model_validator(mode='before')
+    @model_validator(mode="before")
     @classmethod
     def validate_toolkit(cls, values):
-        base_url = values.get('base_url')
-        token = values.get('token')
+        base_url = values.get("base_url")
+        token = values.get("token")
         cls._client = ZephyrClient(base_url=base_url, token=token)
         return values
 
     def get_test_case(self, testcase_id: str):
-
         """Retrieve test case data by id."""
         try:
             return self._client.get_test_case(testcase_id)
@@ -29,7 +28,6 @@ class ZephyrApiWrapper(BaseToolApiWrapper):
             return ToolException(f"Unable to retrieve test cases: {e}")
 
     def search_zql(self, zql_json: str):
-
         """Retrieve Zephyr entities by zql."""
         try:
             return self._client.search_by_zql(zql_json)
@@ -37,12 +35,33 @@ class ZephyrApiWrapper(BaseToolApiWrapper):
             return ToolException(f"Unable to retrieve Zephyr entities: {e}")
 
     def create_testcase(self, create_testcase_json: str):
-
-        """Retrieve Zephyr entities by zql."""
+        """Creates Zephyr test case."""
         try:
             return self._client.create_testcase(create_testcase_json)
         except Exception as e:
-            return ToolException(f"Unable to retrieve Zephyr entities: {e}")
+            return ToolException(f"Unable to create Zephyr entities: {e}")
+
+    def create_teststep(
+        self, testcase_id: str, testcase_tree_id: str, create_teststep_json: str
+    ):
+        """Creates Zephyr test case step."""
+        try:
+            return self._client.create_teststep(
+                testcase_id, testcase_tree_id, create_teststep_json
+            )
+        except Exception as e:
+            return ToolException(f"Unable to create Zephyr test step: {e}")
+
+    def update_teststep(
+        self, testcase_id: str, testcase_tree_id: str, update_teststep_json: str
+    ):
+        """Updates Zephyr test case step."""
+        try:
+            return self._client.update_teststep(
+                testcase_id, testcase_tree_id, update_teststep_json
+            )
+        except Exception as e:
+            return ToolException(f"Unable to update Zephyr test step: {e}")
 
     def get_available_tools(self):
         return [
@@ -51,7 +70,7 @@ class ZephyrApiWrapper(BaseToolApiWrapper):
                 "description": self.get_test_case.__doc__,
                 "args_schema": create_model(
                     "GetTestCasesModel",
-                    testcase_id=(str, Field(description="The ID of the testcase"))
+                    testcase_id=(str, Field(description="The ID of the testcase")),
                 ),
                 "ref": self.get_test_case,
             },
@@ -60,11 +79,15 @@ class ZephyrApiWrapper(BaseToolApiWrapper):
                 "description": self.search_zql.__doc__,
                 "args_schema": create_model(
                     "SearchZqlModel",
-                    zql_json=(str, Field(description=
-                                         """
+                    zql_json=(
+                        str,
+                        Field(
+                            description="""
                                          ZQL json query, i.e.
                                          `{"entitytype": "testcase","word": "name ~ \"Desktop.AEM.Booking\""}`
-                                         """))
+                                         """
+                        ),
+                    ),
                 ),
                 "ref": self.search_zql,
             },
@@ -73,13 +96,51 @@ class ZephyrApiWrapper(BaseToolApiWrapper):
                 "description": self.create_testcase.__doc__,
                 "args_schema": create_model(
                     "CreateTestcaseModel",
-                    create_testcase_json=(str, Field(description=
-                                         """
+                    create_testcase_json=(
+                        str,
+                        Field(
+                            description="""
                                          JSON body of create test case query, i.e.
                                          `{ "tcrCatalogTreeId": 137973, "testcase": { "name": "TestToolkit", 
                                          "description": "some description", "projectId": 75 } }`
-                                         """))
+                                         """
+                        ),
+                    ),
                 ),
                 "ref": self.create_testcase,
-            }
+            },
+            {
+                "name": "create_teststep",
+                "description": self.create_teststep.__doc__,
+                "args_schema": create_model(
+                    "CreateTestcaseStepModel",
+                    create_teststep_json=(
+                        str,
+                        Field(
+                            description="""
+                                         JSON body of create test case step request, i.e.
+                                         `{"tcId":199,"maxId":1,"step":{"step":"login to application","data":"enter credentials","result":"login successful","orderId":1},"tctId":215}`
+                                         """
+                        ),
+                    ),
+                ),
+                "ref": self.create_teststep,
+            },
+            {
+                "name": "update_teststep",
+                "description": self.update_teststep.__doc__,
+                "args_schema": create_model(
+                    "UpdateTestcaseStepModel",
+                    update_teststep_json=(
+                        str,
+                        Field(
+                            description="""
+                                         JSON body of update test case step request, i.e.
+                                         `{"id":4,"maxId":1,"maxVersionNumber":1,"projectId":1,"releaseId":1,"step":{"customFieldProcessed":true,"customFieldValues":[],"customProcessedProperties":{},"customProperties":{},"data":"enter credentials update ","id":7,"localId":1,"orderId":1,"result":"login successful update","step":"login to application update"},"steps":[],"tcId":6,"tctId":7,"testcaseVersionId":6}`
+                                         """
+                        ),
+                    ),
+                ),
+                "ref": self.update_teststep,
+            },
         ]
--- src/alita_tools/zephyr_enterprise/zephyr_enterprise.py
+++ src/alita_tools/zephyr_enterprise/zephyr_enterprise.py
@@ -17,7 +17,9 @@ class ZephyrClient:
         self.base_url = base_url.rstrip("/")  # Ensure no trailing slash
         self.auth_header = self._generate_auth_header(token=token)
 
-    def _generate_auth_header(self, token: str = None, username: str = None, password: str = None):
+    def _generate_auth_header(
+        self, token: str = None, username: str = None, password: str = None
+    ):
         """
         Generate the Basic Authentication header.
 
@@ -28,7 +30,9 @@ class ZephyrClient:
         """
 
         if not (token or (username and password)):
-            raise ToolException("You have to declare token or username with password for authentication")
+            raise ToolException(
+                "You have to declare token or username with password for authentication"
+            )
 
         if token:
             return {"Authorization": f"Bearer {token}"}
@@ -64,31 +68,106 @@ class ZephyrClient:
             raise Exception(f"API request failed: {e}")
 
     def get_test_case(self, testcase_id: str):
-
         """
         Retrieve test case information by id
 
         :param testcase_id: The ID of the test case.
         :return: Test case information.
         """
-        return self._request("GET", f"/flex/services/rest/latest/testcase/{testcase_id}")
+        return self._request(
+            "GET", f"/flex/services/rest/latest/testcase/{testcase_id}"
+        )
 
     def search_by_zql(self, zql_json: str):
-
         """
         Retrieve Zephyr entities by zql
 
         :param zql_json: ZQL json.
         :return: List of entities.
         """
-        return self._request("POST", f"/flex/services/rest/latest/advancesearch/zql", data=json.loads(zql_json))
+        return self._request(
+            "POST",
+            "/flex/services/rest/latest/advancesearch/zql",
+            data=json.loads(zql_json),
+        )
 
     def create_testcase(self, create_testcase_json: str):
-
         """
         Creates new test case per given create_testcase_json fields
 
         :param create_testcase_json: JSON string testcase entity.
         :return: Test case data.
         """
-        return self._request("POST", f"/flex/services/rest/latest/testcase/", data=json.loads(create_testcase_json))
+        return self._request(
+            "POST",
+            "/flex/services/rest/latest/testcase/",
+            data=json.loads(create_testcase_json),
+        )
+
+    def create_teststep(
+        self, testcase_id: str, testcase_tree_id: str, create_teststep_json: str
+    ):
+        """
+        Creates new test case step with given create_teststep_json fields
+
+        :param testcase_version_id: ID of the test case version.
+        :param testcase_tree_id: test case ID in the test case tree.
+        :param create_teststep_json: JSON string testcase step entity. For example:
+          {
+            "tcId": 199,
+            "maxId": 1,
+            "step": {
+              "step": "login to application",
+              "data": "enter credentials",
+              "result": "login successful",
+              "orderId": 1
+            },
+            "tctId": 215
+          }
+        :return: Test case data.
+        """
+        return self._request(
+            "POST",
+            f"/flex/services/rest/latest/testcase/{testcase_id}/teststep/detail/{testcase_tree_id}",
+            data=json.loads(create_teststep_json),
+        )
+    
+    def update_teststep(
+        self, testcase_id: str, testcase_tree_id: str, update_teststep_json: str
+    ):
+        """
+        Updates test case step with given update_teststep_json fields
+
+        :param testcase_version_id: ID of the test case version.
+        :param testcase_tree_id: test case ID in the test case tree.
+        :param update_teststep_json: JSON string testcase step entity. For example:
+          {
+            "id": 4,
+            "maxId": 1,
+            "maxVersionNumber": 1,
+            "projectId": 1,
+            "releaseId": 1,
+            "step": {
+              "customFieldProcessed": true,
+              "customFieldValues": [],
+              "customProcessedProperties": {},
+              "customProperties": {},
+              "data": "enter credentials update ",
+              "id": 7,
+              "localId": 1,
+              "orderId": 1,
+              "result": "login successful update",
+              "step": "login to application update"
+            },
+            "steps": [],
+            "tcId": 6,
+            "tctId": 7,
+            "testcaseVersionId": 6
+          }
+        :return: Test case data.
+        """
+        return self._request(
+            "PUT",
+            f"/flex/services/rest/latest/testcase/{testcase_id}/teststep/detail/{testcase_tree_id}",
+            data=json.loads(update_teststep_json),
+        )
