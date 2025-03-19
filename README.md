2f7bba356a412f5acbdea80c42b6ab4cf61c4895
nochore <40186790+nochore@users.noreply.github.com>
2025-03-19 08:56:20 +0400

    Feat(add): Introduce Pytests

--- README.md
+++ README.md
@@ -27,3 +27,39 @@ Expected result is linked **_alita_tools_** folder in project structure.
 |-- ...  
 |-- **src**  
 |-- ...  
+
+# PyTest
+### Dependencies
+- pytest
+- allure-pytest==2.13.5
+
+### How to run
+
+Install all dependencies:
+
+```bash
+python -m venv .venv && . .venv/bin/activate &&
+python -m pip install --upgrade pip
+pip install -r requirements.txt
+pip install -r dev-requirements.txt
+pip install pytest coverage allure-pytest
+```
+
+Run tests:
+```bash
+pytest .
+```
+
+Open Allure report:
+```bash
+allure open docs -h localhost
+```
+where: docs folder is generated in `conftest.py` automatically from `allure-results` which are being removed during execution
+
+Run Coverage report:
+```bash
+coverage run -m pytest &&
+coverage report &&
+coverage html --show-context
+```
+Open `<full_directory_path>/htmlcov/index.html` in the browser
---
+++ src/tests/conftest.py
@@ -0,0 +1,66 @@
+import os
+import shutil
+import subprocess
+import sys
+
+import pytest
+
+ALLURE_RESULTS_DIR = "allure-results"
+ALLURE_REPORT_DIR = "docs"
+
+
+@pytest.fixture(scope="session")
+def check_env_vars(env_vars):
+    """Ensure all required environment variables are set."""
+    missing_vars = [var.name for var in env_vars if var.get_value() is None]
+    if missing_vars:
+        pytest.fail(f"Required environment variables are not set: {missing_vars}\n")
+
+
+@pytest.hookimpl(tryfirst=True)
+def pytest_configure(config):
+    abs = os.path.abspath("src")
+    if abs not in sys.path:
+        sys.path.insert(0, abs)
+    if not os.path.exists(ALLURE_RESULTS_DIR):
+        os.makedirs(ALLURE_RESULTS_DIR)
+    config.option.allure_report_dir = ALLURE_RESULTS_DIR
+
+
+@pytest.fixture(scope="session", autouse=True)
+def test_suite_cleanup():
+    yield
+
+
+@pytest.hookimpl(trylast=True)
+def pytest_sessionfinish(session, exitstatus):
+    print(f"\nTest suite finished.\nExit status: {exitstatus}")
+    if not os.getenv("GITHUB_ACTIONS"):
+        generate_allure_report()
+
+
+def generate_allure_report():
+    if os.path.isdir(ALLURE_RESULTS_DIR):
+        os.makedirs(ALLURE_REPORT_DIR, exist_ok=True)
+        try:
+            subprocess.run(
+                [
+                    "allure",
+                    "generate",
+                    ALLURE_RESULTS_DIR,
+                    "--output",
+                    ALLURE_REPORT_DIR,
+                    "-c",
+                ],
+                check=True,
+            )
+            print("Allure report generated.")
+        except subprocess.CalledProcessError as e:
+            print(f"Failed to generate Allure report. Error: {str(e)}")
+        finally:
+            shutil.rmtree(ALLURE_RESULTS_DIR)
+            print(f"Cleaned up '{ALLURE_RESULTS_DIR}' directory.")
+    else:
+        print(
+            f"No Allure results found for generating report in '{ALLURE_RESULTS_DIR}'."
+        )
